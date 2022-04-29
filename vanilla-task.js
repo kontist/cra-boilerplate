a796c3a1750419a1822a03496072aca0b45abfaf
Alexander Modes <alexander.modes@kontist.com>
2022-04-29 12:40:58 +0200

    Add vanilla js task

---
+++ vanilla-task.js
@@ -0,0 +1,15 @@
+// you can execute this file by running the command  "node vanilla-task.js" from terminal 
+
+
+const arrayOfObjects = [
+  { transactionId: 1, amount: 150, iban: "DE123456" },
+  { transactionId: 2, amount: 350, iban: "DE564321" },
+  { transactionId: 3, amount: 250, iban: "DE473213" },
+];
+
+const run = () => {
+  // create a new array that just includes the transactionIds from the
+  // arrayOfObjects ([1,2,3]) and console.log it
+};
+
+run();
